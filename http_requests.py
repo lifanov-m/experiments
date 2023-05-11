8af88232c773632d3408ab96da392f07c1f42c20
Lifanov <lifanov-m@mail.ru>
2023-05-11 15:36:22 +0300

    modified:   http_requests.py

--- http_requests.py
+++ http_requests.py
@@ -7,8 +7,23 @@ Created on Thu May 11 13:59:18 2023
 """
 
 import requests
+from requests.exceptions import HTTPError
 
-requests.get('https://api.github.com')
-#<Response [200]>
 
-requests.
+#response = requests.get('https://api.github.com')
+
+for url in ['https://api.github.com' ,'https://api.github.com/invalid']:
+    try:
+        response = requests.get(url)
+
+        response.raise_for_status()
+        print(response.status_code)
+        
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    else:
+        print('Success')
+        
+
